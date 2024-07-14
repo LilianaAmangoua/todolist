85d65e20d3bd361997cd15425c7e6b4dfc44275a
Liliana A. <amangoua99@gmail.com>
2024-07-14 11:29:01 +0200

    V4- Ajout fonction ajouter tâche

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 //Personnaliser le 'Bonjour'
-window.addEventListener ('load', function() {
+/*window.addEventListener ('load', function() {
     let prenom = window.prompt("Quel est votre prénom?");
 
     if(prenom) {
@@ -10,7 +10,7 @@ window.addEventListener ('load', function() {
     else {
         alert ('Vous n\'avez pas saisi de prénom.')
     }
-});
+});*/
 
 //Afficher la date du jour
 const date = new Date();
@@ -38,16 +38,47 @@ iconesSupprimer.forEach(function(icone) {
     });
 });
 
-//Ajouter une tache
-/*let boutonAjouter = document.querySelector('#boutonAjouter');
-let ajouterUneTache = document.querySelector('#ajouterUneTache').value;
+//Créer une nouvelle tache
+
+let boutonAjouter = document.getElementById('boutonAjouter');
 
 boutonAjouter.addEventListener('click', function() {
-    if (ajouterUneTache === ''){
-        alert('Saisissez une tache');
+    let ajouterUneTache = document.querySelector('#ajouterUneTache').value;
+
+    if (ajouterUneTache === '') {
+        alert ("Saisissez une tache s'il vous plait");
     }
 
     else {
+        let container = document.querySelector('.liste-taches');
+        let nouvelleTache = document.createDocumentFragment();
+        
+        const label = document.createElement('label');
+        label.className = 'form-control';
 
+        const tacheSaisie = document.createTextNode(ajouterUneTache);
+        
+        const input = document.createElement('input');
+        input.type = 'checkbox';
+        input.name = 'checkbox';
+        
+        const i = document.createElement('i');
+        i.className='fa-solid fa-trash';
+
+        i.addEventListener('click', function() {
+            let tacheASupprimer = this.closest('.form-control');
+            if (tacheASupprimer) {
+                tacheASupprimer.remove();
+            }
+        });
+        
+        label.appendChild(input);
+        label.appendChild(tacheSaisie);
+        label.appendChild(i);
+        
+        nouvelleTache.appendChild(label);
+        container.appendChild(nouvelleTache);
+
+        document.querySelector('#ajouterUneTache').value = '';
     }
-}*/
+});
